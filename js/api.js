716baffe8a9270067920855b21e698e88461cfa8
lucaianl <lucaianml@gmail.com>
2023-03-18 15:50:03 -0300

    Base do randomFilms

---
+++ js/api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'c0a418bb63f2bbc43037de3e0c54b951';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import {
+    API_KEY, BASE_URL,
+    IMG_URL,
+    language,
+} from './api.js'
+
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', print)
+// console.log(button)
+
+function print() {
+    console.log('print');
+}
