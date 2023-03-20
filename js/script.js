1791df11a7ed70ef0f3027ffd6dd0da0c256cd39
lucaianl <lucaianml@gmail.com>
2023-03-20 14:50:30 -0300

    API de themoviedb conectada

--- js/script.js
+++ js/script.js
@@ -4,15 +4,36 @@ import {
     language,
 } from './api.js'
 
-
+const url = BASE_URL;
 
 const button = document.querySelector('.button');
 const changeToDisplayVisible = document.querySelector('.film');
 const changeToDisplayHidden = document.querySelector('.container')
 
-button.addEventListener('click', seeFilmDescripton)
+button.addEventListener('click', seeFilm)
 
-function seeFilmDescripton() {
+function seeFilm() {
     changeToDisplayVisible.style.display = "";
     changeToDisplayHidden.style.display = "none";
 }
+
+function getTitle() {
+    axios.get(url)
+        .then(response => {
+            const data = response.data.title
+            filmTitle.textContent = JSON.stringify(data)
+        })
+        .catch(error => console.log(error))
+}
+getTitle();
+
+// overview
+function getOverview() {
+    axios.get(url)
+        .then(response => {
+            const data = response.data.overview
+            overview.textContent = JSON.stringify(data)
+        })
+        .catch(error => console.log(error))
+}
+getOverview();
